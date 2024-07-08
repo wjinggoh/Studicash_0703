2e79d5f5192e3f0f349678859ffdf0b6e5836a5f
wjinggoh <wjingggoh15@gmail.com>
2024-07-08 12:34:29 +0800

    08072024-12:34pm

--- app/src/main/java/my/edu/tarc/studicash_0703/Fragment/ChooseTransactionFragment.kt
+++ app/src/main/java/my/edu/tarc/studicash_0703/Fragment/ChooseTransactionFragment.kt
@@ -2,14 +2,12 @@ package my.edu.tarc.studicash_0703.Fragment
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import my.edu.tarc.studicash_0703.AddExpenseActivity
 import my.edu.tarc.studicash_0703.AddIncomeActivity
-import my.edu.tarc.studicash_0703.R
 import my.edu.tarc.studicash_0703.databinding.FragmentChooseTransactionBinding
 
 class ChooseTransactionFragment : DialogFragment() {
---
+++ app/src/main/java/my/edu/tarc/studicash_0703/Models/Income.kt
@@ -0,0 +1,10 @@
+package my.edu.tarc.studicash_0703.Models
+
+data class Income(
+    val incomeTitle: String? = null,
+    val incomeAmount: Double? = null,
+    val date: String? = null,
+    val category: String? = null,
+    val paymentMethod: String? = null,
+    val userId: String? = null
+)
